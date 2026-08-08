94632a6bdfadd2ee3dea0ad98dd64fd4db369ad2
Abhinav Rayabarapu <rayabarapuabhinav@gmail.com>
2026-08-08 20:38:17 +0530

    added db

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'USER' : env('USER'),
+        'PASSWORD' : env('PASSWORD'),
+        'HOST':env('HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
