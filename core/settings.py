1ce0ff88a558fe082175828ed359c84d59c7d192
Abhinav Rayabarapu <rayabarapuabhinav@gmail.com>
2026-08-11 19:34:34 +0530

    smtp conn

--- core/settings.py
+++ core/settings.py
@@ -23,10 +23,10 @@ environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')#True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["*"]
 
