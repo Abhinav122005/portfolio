26579338123c393abf29b6103bac7ddb61d292fb
Abhinav Rayabarapu <rayabarapuabhinav@gmail.com>
2026-08-12 12:33:07 +0530

    fix error

--- core/settings.py
+++ core/settings.py
@@ -137,3 +137,4 @@ EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
 EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=False)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='')  
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='')
+EMAIL_TIMEOUT = 5  # Add a 5-second timeout to prevent Gunicorn crashes
